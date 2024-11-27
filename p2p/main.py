3ea97b0f3c2ad7e94a3e49dfc86c612dfe799fae
Marck <38380537+Mark-48@users.noreply.github.com>
2024-11-27 23:47:17 +0300

    Update main.py

--- p2p/main.py
+++ p2p/main.py
@@ -9,36 +9,10 @@ import random
 import webbrowser
 
 
-
-def write_data(data):
-        current_date = datetime.datetime.now().strftime('%d-%m-%Y')
-
-        conn = sqlite3.connect('zalupaData.db')
-        cur = conn.cursor()
-        r = random.randint(0,22)
-        DATA = (r,current_date)
-
-
-        for i in data: DATA = DATA + tuple({data[i]})
-
-        print(DATA)
-        cur.execute("INSERT INTO manager VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?);", DATA)
-        conn.commit()
-
-
-def read_data():
-        h = []
-        conn = sqlite3.connect('zalupaData.db')
-        cur = conn.cursor()
-        cur.execute("SELECT * FROM manager WHERE idu = 21;")
-        one_result = cur.fetchone()
-
-        for i in one_result[2:]: h.append(i)
-
-        return h
-
-
 def write_json(data_set = 0):
+        """ 
+        Функция для записи в файл JSON
+        """
         data = data_set
         with open('info.json','w') as of:
                 json.dump(data,of)
@@ -46,6 +20,9 @@ def write_json(data_set = 0):
 
 
 def read_json():
+        """ 
+        Функция для чтения файл JSON
+        """
         with open("info.json") as fh:
                 dater = json.load(fh)
 
@@ -53,18 +30,16 @@ def read_json():
 
 app = Flask(__name__)
 
-
-@app.route('/')
+@app.route('/') #Главная основная страница
 def index():
-        #, pn = d[0], vt=d[1], sr = d[2], cht = d[3], pt = d[4], sb = d[5], vs = d[6]
+        
         return render_template('hello.html')
 
-@app.route('/dev')
+@app.route('/dev') # Шуточная страница, для прикола, это некий DEV-Vlog
 def upd_page():
-        #, pn = d[0], vt=d[1], sr = d[2], cht = d[3], pt = d[4], sb = d[5], vs = d[6]
         return render_template('upd.html')
 
-@app.route('/data_post', methods=['POST'])
+@app.route('/data_post', methods=['POST'])  # страница для общения с JS
 def data_inside_post():
 
         re = request.json
@@ -72,7 +47,7 @@ def data_inside_post():
 
         return "A?"
 
-@app.route('/data_get')
+@app.route('/data_get')  # страница для общения с JS
 def data_inside_get():
         g = read_json()
 
@@ -81,5 +56,5 @@ def data_inside_get():
 
 
 if __name__ == '__main__':
-        webbrowser.open('http://127.0.0.1:5000/')
+        webbrowser.open('http://127.0.0.1:5000/') # для удобства, чтобы запустить все одним кликом.
         app.run(debug=True)
